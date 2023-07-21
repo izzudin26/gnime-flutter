1ce28be1d20986aff85d71e36d6bd4cb7d87b0a5
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-07-22 01:45:58 +0700

    add search_anime_card

---
+++ lib/Component/search_anime_card.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gogonime/Model/search_anime.dart';
+import 'package:flutter_gogonime/Screen/detail_anime.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:flutter_gogonime/Colors.dart' as kcolors;
+import 'package:flutter_gogonime/Style/text.dart' as ktext;
+
+class SearchAnimeCard extends StatelessWidget {
+  final SearchAnime searchAnime;
+  const SearchAnimeCard({super.key, required this.searchAnime});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    DetailAnimeScreen(imageUrl: searchAnime.animeImg, id: searchAnime.animeId, animeTitle: searchAnime.animeTitle)));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 7),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Image.network(
+                searchAnime.animeImg,
+                height: 180,
+                width: 130,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Shimmer.fromColors(
+                    baseColor: Colors.grey,
+                    highlightColor: Colors.grey.shade300,
+                    child: Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: kcolors.bgSlate),
+                      width: 130,
+                      height: 180,
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ktext.kTitleDetail(searchAnime.animeTitle),
+                const SizedBox(
+                  height: 15,
+                ),
+                ktext.kSubtitle(searchAnime.status)
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
