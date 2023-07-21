13571b34178c05d9931b21f037d31e490888e4d6
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-07-22 01:46:27 +0700

    complete search screen

--- lib/Screen/search.dart
+++ lib/Screen/search.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gogonime/Colors.dart' as kcolors;
 import 'package:flutter_gogonime/Component/form.dart' as kform;
+import 'package:flutter_gogonime/Component/search_anime_card.dart';
+import 'package:flutter_gogonime/Component/search_shimmer.dart';
+import 'package:flutter_gogonime/Notifier/Anime.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends ConsumerStatefulWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
+  SearchScreenState createState() => SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen> {
+class SearchScreenState extends ConsumerState<SearchScreen> {
   @override
   Widget build(BuildContext context) {
+    final anime = ref.watch(animeProvider);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -23,7 +29,42 @@ class _SearchScreenState extends State<SearchScreen> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
         child: Column(
-          children: [kform.kTextField()],
+          children: [
+            kform.kTextField(
+                initialValue: anime.searchKeyword,
+                onChange: (val) {
+                  anime.setSearchKeyword(val);
+                  anime.getSearchData();
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: size.height * .74,
+              child: anime.isSearchLoading
+                  ? ListView.builder(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      primary: false,
+                      physics: const ScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (context, _) {
+                        return const SearchLoadingShimmer();
+                      },
+                    )
+                  : ListView.builder(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      primary: false,
+                      physics: const ScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: anime.searchAnime.length,
+                      itemBuilder: (context, index) {
+                        final currentAnime = anime.searchAnime[index];
+                        return SearchAnimeCard(searchAnime: currentAnime);
+                      },
+                    ),
+            )
+          ],
         ),
       ),
     );
