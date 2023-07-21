da4a0b3b6228102018484a78403043716c4da7cd
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-07-22 01:06:12 +0700

    add param onChange

--- lib/Component/form.dart
+++ lib/Component/form.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gogonime/Colors.dart' as kcolors;
 import 'package:lucide_icons/lucide_icons.dart';
 
-TextField kTextField({TextEditingController? controller}) {
+TextField kTextField({TextEditingController? controller, void Function(String)? onChange}) {
   return TextField(
     controller: controller,
     style: const TextStyle(color: kcolors.primary),
+    onChanged: onChange,
     decoration: InputDecoration(
         contentPadding: EdgeInsets.zero,
         prefixIcon: const Icon(LucideIcons.search, color: kcolors.primary),
