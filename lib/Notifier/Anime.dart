7148eb1c38bdaeaed5ac1756a14f3ee11ba4369c
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-07-22 01:06:00 +0700

    add method search

--- lib/Notifier/Anime.dart
+++ lib/Notifier/Anime.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gogonime/Model/detail_anime.dart';
 import 'package:flutter_gogonime/Model/popular_anime.dart';
 import 'package:flutter_gogonime/Model/recent_episode.dart';
+import 'package:flutter_gogonime/Model/search_anime.dart';
 import 'package:flutter_gogonime/Repository/gogonime.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,8 +15,17 @@ class AnimeNotifier extends ChangeNotifier {
   int recentEpisodePage = 1;
   int popularPage = 1;
   bool isLoading = false;
+  bool isSearchLoading = false;
+
+  String searchKeyword = "";
   List<RecentEpisode> recentEpisodes = [];
   List<PopularAnime> popularAnime = [];
+  List<SearchAnime> searchAnime = [];
+
+  void setSearchKeyword(String val) {
+    searchKeyword = val;
+    notifyListeners();
+  }
 
   Future<void> fetchRecentEpisodes() async {
     isLoading = true;
@@ -43,5 +53,15 @@ class AnimeNotifier extends ChangeNotifier {
     final repo = ref.read(gogonimeRepositoryProvider);
     final data = await repo.detailAnime(animeId: animeId);
     return data;
+  }
+
+  Future<void> getSearchData() async {
+    isSearchLoading = true;
+    notifyListeners();
+    final repo = ref.read(gogonimeRepositoryProvider);
+    final data = await repo.getSearchAnime(searchKeyword);
+    searchAnime = data;
+    isSearchLoading = false;
+    notifyListeners();
   }
 }
