124b7958deee703413c8435dcfb8ce4a3a75c31f
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2023-07-23 15:11:37 +0700

    add hive model adaptor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gogonime/Model/detail_anime.dart';
 import 'package:flutter_gogonime/Screen/home.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDirectory.path);
+  Hive.registerAdapter(AnimeAdapter());
+  Hive.registerAdapter(EpisodesListAdapter());
   runApp(ProviderScope(child: const MyApp()));
 }
 
